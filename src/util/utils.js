e0fb40d3db57bc1ba70a2f0dd28158d86cef624c
michaelmcguiness <michaelmcguiness123@gmail.com>
2020-05-22 07:35:50 -0400

    adding leaderboard

--- src/util/utils.js
+++ src/util/utils.js
@@ -1,4 +1,8 @@
 export const getNumberWithOrdinal = (n) => {
+  n = parseFloat(n);
+  if (isNaN(n)) {
+    return "n/a";
+  }
   const s = ["th", "st", "nd", "rd"],
     v = n % 100;
   return n + (s[(v - 20) % 10] || s[v] || s[0]);
