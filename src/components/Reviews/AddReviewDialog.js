5ee5c9d3cb9bb483258831426df66874b617e2bc
michaelmcguiness <michaelmcguiness123@gmail.com>
2020-06-05 19:53:45 -0400

    fixed textfield

--- src/components/Reviews/AddReviewDialog.js
+++ src/components/Reviews/AddReviewDialog.js
@@ -8,7 +8,7 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
-import TextField from "@material-ui/core/Textfield";
+import TextField from "@material-ui/core/TextField";
 import FormControl from "@material-ui/core/FormControl";
 import Rating from "@material-ui/lab/Rating";
 import Typography from "@material-ui/core/Typography";
